d34437f45a2cb99762481fec4c8559090e18a5aa
Geremia Taglialatela <tagliala.dev@gmail.com>
2013-01-17 12:00:06 +0100

    Bumped VERSION to 13.01.17 Updated CHANGELOG

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ function optionSkills($start = 0, $stop = 20, $select = 6) {
   }
 }
 ?>
-<?php $staminia_version = "13.01.16" ?>
+<?php $staminia_version = "13.01.17" ?>
 <!DOCTYPE html>
 <html lang="<?php echo localize("lang"); ?>">
   <head>
