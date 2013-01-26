8cbf259295eac61e460057638333352effa78e9f
Geremia Taglialatela <tagliala.dev@gmail.com>
2013-01-26 15:51:10 +0100

    Get webfonts in a single request

--- index.php
+++ index.php
@@ -71,8 +71,7 @@ function optionSkills($start = 0, $stop = 20, $select = 6) {
 
     <!-- Le styles -->
     <link href="css/main.css" rel="stylesheet">
-    <link href="http://fonts.googleapis.com/css?family=Telex" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Telex|Lobster" rel="stylesheet" type="text/css">
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="img/staminia_favicon.png">
