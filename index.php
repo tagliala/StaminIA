1ce2a0437232c68d46a6ac68bef26b362564b2ab
Geremia Taglialatela <tagliala.dev@gmail.com>
2026-02-24 22:05:20 +0100

    Fix version

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ function optionSkills($start = 0, $stop = 20, $select = 6)
     }
 }
 ?>
-<?php $staminia_version = "26.02.23" ?>
+<?php $staminia_version = "26.2.23" ?>
 <!DOCTYPE html>
 <html lang="<?php echo localize("lang"); ?>">
   <head>
